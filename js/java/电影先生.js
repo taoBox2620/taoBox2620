90cb91f32efdc69d443af0db1ec0c42210663e91
taoBox2620 <1342028740@qq.com>
2026-07-30 11:03:19 +0800

    Add files via upload

---
+++ js/java/电影先生.js
@@ -0,0 +1,9 @@
+{
+        "副标题": "电影先生",
+        "请求头": "User-Agent$MOBILE_UA",
+        "编码": "UTF-8",
+        "主页url": "https://silidm.com/",
+        "线路标题": "电影先生",
+        "分类url": "https://silidm.com/show/{cateId}-{area}-{by}-{class}-{lang}-{letter}---{catePg}---{year}.html",
+        "分类": "电影$dianying#剧集$juji#动漫$dongman#综艺$zongyi"
+      }
